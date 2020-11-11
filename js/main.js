939c5cf1ac95ff9aba1eb34e5a52ffd045cba444
SergBY <57902822+SergBY@users.noreply.github.com>
2020-11-11 16:40:14 +0300

    feat: add registration

--- js/main.js
+++ js/main.js
@@ -11,3 +11,61 @@ menuToggle.addEventListener('click', function (event) {
 });
 
 
+const loginElem = document.querySelector('.login'),
+      loginForm = document.querySelector('.login-form'),
+      emailInput = document.querySelector('.login-email'),
+      passwordInput = document.querySelector('.login-password'),
+      loginSignup = document.querySelector('.login-signup');
+// console.log(loginElem);
+
+const listUsers = [
+  {
+    id: '01',
+    email: 'max@mail.ru',
+    password: '12345',
+    displayName: 'MaxJS'
+  },
+  {
+    id: '02',
+    email: 'serg@mail.ru',
+    password: '54321',
+    displayName: 'SergJS'
+  },
+];
+
+const setUsers = {
+  user: null,
+  logIn(email, password) {
+    console.log('logIn');
+  },
+  logOut() {
+    console.log('logOut');
+  },
+  signUp(email, password) {
+    console.log('signUp');
+    if (!this.getUser(email)) {
+      listUsers.push({email, password, displayName: email});
+    } else {
+      alert("Пользователь с таким именем уже есть");
+    }
+  }
+};
+// setUsers.logIn();
+// setUsers.logOut();
+// setUsers.signUp();
+
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  // const emailValue = emailInput.value,
+  //       passwordValue = passwordInput.value;
+  // setUsers.logIn(emailValue, passwordValue);
+  setUsers.logIn(emailInput.value, passwordInput.value);  //замена прошлых 3-х
+});
+loginSignup.addEventListener('click', (event) => {
+  event.preventDefault();
+    // const emailValue = emailInput.value,
+  //       passwordValue = passwordInput.value;
+  // setUsers.signUp(emailValue, passwordValue);
+  setUsers.signUp(emailInput.value, passwordInput.value); //замена прошлых 3-х
+});
+
